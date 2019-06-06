635093ca329a4a229cbb3e8521a50f2960ea0060
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2019-06-06 13:37:01 +0200

    Pet Clinic init

--- src/main/java/me/suveen/revision/repositories/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package me.suveen.revision.repositories;
-
-import me.suveen.revision.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {}
--- src/main/java/me/suveen/revision/repositories/PublisherRepository.java
+++
@@ -1,6 +0,0 @@
-package me.suveen.revision.repositories;
-
-import me.suveen.revision.model.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher, Long> {}
